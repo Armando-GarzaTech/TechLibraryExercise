c10fdd591f7c9aaa13e75ae1a1b7787165bfc576
Armando Garza <mondog2k@msn.com>
2020-11-21 00:23:42 -0800

    add: book edit and update feature

--- TechLibrary.Web/src/components/Book.vue
+++ TechLibrary.Web/src/components/Book.vue
@@ -7,11 +7,23 @@
                 tag="article"
                 style="max-width: 30rem;"
                 class="mb-2">
-            <b-card-text>
-                {{ book.descr }}
+            <b-card-text style="min-height: 150px;">
+                <div v-if="editing">
+                  <b-form-textarea v-model="book.descr" rows="4">
+                  </b-form-textarea> 
+                </div>
+                <div v-else class="textbox">
+                  {{ book.descr }}
+                </div>
             </b-card-text>
 
-            <b-button to="/" variant="primary">Back</b-button>
+          <b-row align-v="center">
+            <b-col>
+              <b-button to="/" variant="primary">Back</b-button>
+              <b-button variant="success" @click="save" v-if="editing">Save</b-button>
+            </b-col>
+            <b-col><b-form-checkbox v-model="editing" switch size="lg">Edit</b-form-checkbox></b-col>
+          </b-row>
         </b-card>
     </div>
 </template>
@@ -19,17 +31,41 @@
 <script>
     import axios from 'axios';
 
-    export default {
-        name: 'Book',
-        props: ["id"],
-        data: () => ({
-            book: null
-        }),
-        mounted() {
-            axios.get(`https://localhost:5001/books/${this.id}`)
-                .then(response => {
-                    this.book = response.data;
-                });
-        }
+  export default {
+    name: 'Book',
+    props: ["id"],
+    data: () => ({
+      book: null,
+      editing: false,
+    }),
+    mounted() {
+      axios.get(`https://localhost:5001/books/${this.id}`)
+        .then(response => {
+          this.book = response.data;
+        });
+    },
+    methods: {
+      save() {
+        axios.post(`https://localhost:5001/books/update`, this.book)
+          .then(() => {
+            window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/');
+          });
+      },
     }
+
+  }
 </script>
+
+<style scoped>
+  .textbox {
+    padding: 0.375rem 0.75rem;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+  }
+
+  .btn {
+    width: 75px;
+    margin-right: 6px;
+  }
+</style>
